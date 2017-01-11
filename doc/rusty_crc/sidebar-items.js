7d2eeaf508edd4f43252844522db897830009ad5
F3real <stefan92ff@yandex.com>
2017-01-11 20:54:08 +0100

    Updating docs and adding generated doc folder

---
+++ doc/rusty_crc/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["crc",""],["crc_builder",""]]});
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+//! crc16 library 
+//!
+//! #### Installation
+//!
+//! ```toml
+//! [dependencies]
+//! rusty_crc16 = "*"
+//! ```
 pub mod crc;
 pub mod crc_builder;
